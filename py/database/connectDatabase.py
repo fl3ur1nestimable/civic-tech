5e1ed0d788d7a2d500a441db34412b7f223488a2
Thibault <thibault.cheneviere@telecomnancy.eu>
2021-11-27 10:20:12 +0100

    Correction d'une erreur de typage dans le fichier connectDatabase.py

--- py/database/connectDatabase.py
+++ py/database/connectDatabase.py
@@ -4,13 +4,23 @@
     Date : 26/11/2021
 """
 
-# Import neded packages
+# Import needed packages
 import sqlite3
 from sqlite3.dbapi2 import Connection, Cursor
+from typing import Tuple
 
 
 
-def connectDatabase() -> tuple[Connection, Cursor]:
+def connectDatabase() -> Tuple[Connection, Cursor]:
+    """
+        Function that returns db connection and the cursor to interact with the database.db file
+
+        Parameters :
+            None
+
+        Returns :
+            - tuple [Connection, Cursor] : a tuple of the database connection and cursor
+    """
     db = sqlite3.connect('database.db')
     cursor = db.cursor()
     return db, cursor
