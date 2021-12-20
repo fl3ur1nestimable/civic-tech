d9cfe7cae4cfb2aeec41d9728826d2c5f560dd75
Yebouet <antoine.yebouet@telecomnancy.eu>
2021-12-20 12:53:12 +0100

    version 2

--- py/blueprints/progFull.py
+++ py/blueprints/progFull.py
@@ -6,22 +6,35 @@
 
 # Import neded packages
 from flask import Blueprint, session, request
-from flask.helpers import flash
+from flask.helpers import flash, url_for
 from flask.templating import render_template
+from werkzeug.utils import redirect
 
 
 #Import personal modules
 from py.database.connectDatabase import connectDatabase
 from py.database.databaseFunctions import checkValue
-from py.core.truncatePrograms import truncatePrograms
 
 #Definition of the blueprint
 progFull= Blueprint('progFull',__name__)
 
 
-@progFull.route('/program')
-def program():
+@progFull.route('/program0')
+def program0():
     nomCandidat=request.args.get('nomCandidat')
     prenomCandidat=request.args.get('prenomCandidat')
-    prog=request.args.get('prog')
+    data=[nomCandidat,prenomCandidat]
+    db, cursor =connectDatabase()
+    query = '''SELECT  FROM users WHERE ;'''
+
+    session['data']=data
+    return redirect(url_for('progFull.program'))
+
+
+@progFull.route('/program')
+def program():
+    data=session.get('data',None)
+    nomCandidat=data[0]
+    prenomCandidat=data[1]
+    prog=data[2]
     return render_template('program.html',nomCandidat=nomCandidat,prenomCandidat=prenomCandidat,prog=prog)
