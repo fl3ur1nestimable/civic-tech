bfeabf2266ca8739158c74528ffb15ef00871460
Thibault <thibault.cheneviere@telecomnancy.eu>
2021-12-24 11:36:51 +0300

    Modification des données passées dans la page program.html (on passe un dictionnaire a la place de plein de variables séparées).

--- py/blueprints/progFullBP.py
+++ py/blueprints/progFullBP.py
@@ -19,35 +19,46 @@ from py.core.truncatePrograms import truncatePrograms
 progFullBP = Blueprint('progFullBP',__name__)
 
 
-@progFullBP.route('/program/<string:firstName>/<string:lastName>/<int:id>')
+@progFullBP.route('/program/<string:firstName>/<string:lastName>/<int:id>', methods=['GET', 'POST'])
 def program(firstName: str, lastName: str, id: int) -> str:
-    query = '''SELECT l.program, c.listId, c.catchphrase,c.picture FROM Candidate AS c JOIN List AS l ON c.listId=l.id WHERE c.id=?;'''
-    arg = (id, )
+    if request.method == 'GET':
+        query = '''SELECT l.program, c.listId, c.catchphrase,c.picture FROM Candidate AS c JOIN List AS l ON c.listId=l.id WHERE c.id=?;'''
+        arg = (id, )
 
-    db, cursor = connectDatabase()
-    cursor.execute(query, arg)
+        db, cursor = connectDatabase()
+        cursor.execute(query, arg)
 
-    data=cursor.fetchall()
+        data=cursor.fetchall()
 
-    prog = data[0][0]
-    listId = data[0][1]
-    citation=data[0][2]
-    photo=data[0][3]
+        prog = data[0][0]
+        listId = data[0][1]
+        citation=data[0][2]
+        photo=data[0][3]
 
-    db.close()
+        db.close()
 
-    query2='''SELECT g.environment,g.social,g.economy FROM ProgramGrade as g WHERE g.listId=? '''
-    arg2=(listId, )
-    db2, cursor2 = connectDatabase()
-    cursor2.execute(query2,arg2)
-    data2=cursor2.fetchall()
-    grades=[data2[0][0],data2[0][1],data2[0][2]]
-    db2.close()
+        query2='''SELECT g.environment,g.social,g.economy FROM ProgramGrade as g WHERE g.listId=? '''
+        arg2=(listId, )
+        db2, cursor2 = connectDatabase()
+        cursor2.execute(query2,arg2)
+        data2=cursor2.fetchall()
+        grades=[data2[0][0],data2[0][1],data2[0][2]]
+        db2.close()
 
-    query3='''SELECT m.firstName,m.lastName,m.job FROM Member as m WHERE m.listID =?'''
-    db3, cursor3 =connectDatabase()
-    cursor3.execute(query3,arg2)
-    liste_membres=cursor3.fetchall()
-    db3.close()
+        query3='''SELECT m.firstName,m.lastName,m.job FROM Member as m WHERE m.listID =?'''
+        db3, cursor3 =connectDatabase()
+        cursor3.execute(query3,arg2)
+        liste_membres=cursor3.fetchall()
+        db3.close()
 
-    return render_template('program.html',nomCandidat=firstName,prenomCandidat=lastName,prog=prog,grades=grades,citation=citation,membres=liste_membres,photo=photo)
+        candidateDict = {
+            "firstName": firstName,
+            "lastName": lastName,
+            "program": prog,
+            "grades": grades,
+            "catchphrase": citation,
+            "membres": liste_membres,
+            "picture": photo
+        }
+
+        return render_template('program.html', data=candidateDict)
